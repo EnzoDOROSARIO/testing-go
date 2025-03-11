9ee0c1835c4ce62d3fb62bb9d2677a2a848d9c8e
Enzo DO ROSARIO <contact@edorosario.fr>
2025-03-11 22:14:11 +0100

    small refactor on test

--- internal/domain/commands/book_ride/book_ride_test.go
+++ internal/domain/commands/book_ride/book_ride_test.go
@@ -12,30 +12,35 @@ import (
 
 func TestBasicRideBooking(t *testing.T) {
 	testCases := []struct {
+		name          string
 		distance      int
 		expectedPrice float64
 	}{
-		{distance: 0, expectedPrice: 30},
-		{distance: 1, expectedPrice: 30.5},
+		{name: "0km", distance: 0, expectedPrice: 30},
+		{name: "1km", distance: 1, expectedPrice: 30.5},
 	}
 
 	for _, tc := range testCases {
-		rideRepository := ride_repository.NewFakeRideRepository()
-		tripScanner := trip_scanner.NewFakeTripScanner(tc.distance)
-		bookRide := book_ride.NewRideBooker(tripScanner, rideRepository)
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
 
-		bookRide.Execute("rideId", "riderId", "PARIS_ADDRESS", "PARIS_ADDRESS")
+			rideRepository := ride_repository.NewFakeRideRepository()
+			tripScanner := trip_scanner.NewFakeTripScanner(tc.distance)
+			bookRide := book_ride.NewRideBooker(tripScanner, rideRepository)
 
-		actualRide := rideRepository.Rides[0]
+			bookRide.Execute("rideId", "riderId", "PARIS_ADDRESS", "PARIS_ADDRESS")
 
-		expectedSnapshot := ride_booking.RideSnapshot{
-			ID:        "rideId",
-			RiderId:   "riderId",
-			Departure: "PARIS_ADDRESS",
-			Arrival:   "PARIS_ADDRESS",
-			Price:     tc.expectedPrice,
-			Status:    "WAITING_FOR_DRIVER",
-		}
-		assert.Equal(t, expectedSnapshot, actualRide.ToSnapshot())
+			actualRide := rideRepository.Rides[0]
+
+			expectedSnapshot := ride_booking.RideSnapshot{
+				ID:        "rideId",
+				RiderId:   "riderId",
+				Departure: "PARIS_ADDRESS",
+				Arrival:   "PARIS_ADDRESS",
+				Price:     tc.expectedPrice,
+				Status:    "WAITING_FOR_DRIVER",
+			}
+			assert.Equal(t, expectedSnapshot, actualRide.ToSnapshot())
+		})
 	}
 }
